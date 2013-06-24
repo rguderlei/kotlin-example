dd315d92acda3fce189b1c5857f3a798019de8b7
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-06-24 15:18:08 +0200

    rework passing functions as arguments

--- src/test/kotlin/basics/FunctionsTest.kt
+++ src/test/kotlin/basics/FunctionsTest.kt
@@ -10,7 +10,7 @@ class FunctionsTest{
         val a :Int = 1
         val b :Int = 1
 
-       // assertThat(execute(a,b, { add } ), equalTo(add(a,b)))      // does not work, first class functions are not available see KT-1183
-        assertThat(execute(a,b, {(a,b) -> (a+b)}), equalTo(add(a,b)))
+        assertThat(execute(a,b,  ::add  ), equalTo(add_inferred(a,b)))   // pass a function
+        assertThat(execute(a,b, {(a,b) -> (a+b)}), equalTo(add(a,b)))  // pass a function literal
     }
 }
