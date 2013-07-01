ed5dbecd58ea194eefa79d58ae3c2518a02ab753
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-07-01 11:06:01 +0200

    restructure project to fix js compilation, pt.2

--- kotlin-example/src/main/kotlin/modules/MyModule.kt
+++ kotlin-example/src/main/kotlin/modules/MyModule.kt
@@ -1,7 +1,7 @@
 package modules
 
 val homeDir = "..."
-
+/*
 module("de.exxcellent.kotlin.demo") {
     depends(MavenRepo["junit.junit@4.10"])
 
@@ -10,4 +10,4 @@ module("de.exxcellent.kotlin.demo") {
 
     sourceRoot("$homeDir/src/main/kotlin")
     testSourceRoot("$homeDir/src/test/kotlin")
-}
+}    */
