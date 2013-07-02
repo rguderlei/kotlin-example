e4860de310c639c2007723503357925fdd750ff3
Ralph Guderlei <ralph.guderlei@gmail.com>
2013-07-02 09:34:20 +0200

    make js example work

--- kotlin-js-example/src/main/kotlin/Javascript.kt
+++ kotlin-js-example/src/main/kotlin/Javascript.kt
@@ -1,16 +1,19 @@
 package javascript
+import js.jquery.*
+
 
 fun main(args: Array<String>) {
+    // will be automagically called when document is loaded
     jq {
         Kotlin2Javascript().fill()
     }
 }
 
-class Kotlin2Javascript {
-
-
-
+public class Kotlin2Javascript {
     fun fill(){
-
+        jq {
+            val container = jq("#container")
+            container.append("<b>Hello, World!</b>")
+        }
     }
 }
